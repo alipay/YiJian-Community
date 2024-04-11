d35ec57bc6357a4df0f5f18216eef8da01639e1d
xx <lihongcheng.lhc@antgroup.com>
2024-04-11 17:41:09 +0800

    format the files

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -3,7 +3,12 @@ from datasets import load_dataset
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-txt2txt_set = load_dataset("csv", data_files=os.path.join(CURRENT_DIR, "txt2txt/txt2txt.csv"))
-txt2img_set = load_dataset("csv", data_files=os.path.join(CURRENT_DIR, "txt2img/txt2img.csv"))
-img_txt2txt_set = load_dataset("csv", data_files=os.path.join(CURRENT_DIR, "img_txt2txt/img_txt2txt.csv"))
-
+txt2txt_set = load_dataset(
+    "csv", data_files=os.path.join(CURRENT_DIR, "txt2txt/txt2txt.csv")
+)
+txt2img_set = load_dataset(
+    "csv", data_files=os.path.join(CURRENT_DIR, "txt2img/txt2img.csv")
+)
+img_txt2txt_set = load_dataset(
+    "csv", data_files=os.path.join(CURRENT_DIR, "img_txt2txt/img_txt2txt.csv")
+)
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from dataset import txt2txt_set, txt2img_set, img_txt2txt_set
 
 print(txt2txt_set, txt2img_set, img_txt2txt_set)
+
+print(txt2txt_set["train"][:3])
+
+print(txt2img_set["train"][:3])
+
+print(img_txt2txt_set["train"][:3])
