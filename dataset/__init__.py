48fcba73d174859de1b947bbecf3bb9e4af34e68
xx <lihongcheng.lhc@antgroup.com>
2024-04-11 16:55:53 +0800

    delete import of prompt_record.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .prompt_record import Txt2TxtRecord, Txt2ImgRecord, ImgTxt2TxtRecord
 from .utils import risk_category_zh, risk_category_en
 
 __all__ = ["risk_category_zh", "risk_category_en"]
