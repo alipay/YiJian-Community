9a6200111edb468b3d8e52e08392934561af6a44
xx <lihongcheng.lhc@antgroup.com>
2024-04-15 15:52:19 +0800

    initialize model dir

---
+++ model/model_hub.py
@@ -0,0 +1,10 @@
+# models for text to text task
+txt2txt_models = {"open_source": {"Command R+": "CohereForAI/c4ai-command-r-plus"}, "close_source": {}}
+
+
+# models for text to image task
+txt2img_models = {"open_souce": {}, "close_source": {}}
+
+
+# models for image text to text task
+imgtxt2txt_models = {"open_souce": {}, "close_source": {}}
