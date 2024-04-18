d045853b1fa767db80b6ab70cc3c9d4f9f619c83
xx <lihongcheng.lhc@antgroup.com>
2024-04-18 14:30:36 +0800

    refactor

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from utils import *
+
+target_models = ["mistralai/Mistral-7B-Instruct-v0.2", "TinyLlama/TinyLlama-1.1B-Chat-v1.0"]
 
 
 def test_taxonomy():
@@ -77,5 +95,9 @@ def test_txt2txt_inference_on_dataset():
     print(results)
 
 
-test_txt2txt_inference_on_dataset()
+# test_txt2txt_inference_on_dataset()
 
+
+from dataset import imgtxt2txt_set
+
+print(imgtxt2txt_set)
