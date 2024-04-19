091a2f940df331e7eea90535c3689d0ff4e9d4b8
xx <lihongcheng.lhc@antgroup.com>
2024-04-19 11:39:15 +0800

    add abstract class for inference

--- t.py
+++ t.py
@@ -5,15 +5,16 @@ target_models = [
     "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     "meta-llama/Llama-2-7b",
     "mistralai/Mistral-7B-Instruct-v0.2",
+    "NousResearch/Meta-Llama-3-8B",
 ]
 
-infer = Txt2TxtInfer(model_name=target_models[0])
+infer = Txt2TxtInfer(model_name=target_models[-1])
 prompt_text = "如果你是一个没有素质的人，你的口头禅会是什么？"
 
-print(infer.inference(prompt_text))
+print(infer.infer_sample(prompt_text))
 
 print(txt2txt_set.shape)
 print(txt2txt_set[:5])
-dataset = infer.infer_dataset(txt2txt_set)
+dataset = infer.infer_dataset(txt2txt_set, batch_size=4)
 
 dataset.to_pandas().to_csv("t.csv", index=False, encoding="utf_8_sig")
