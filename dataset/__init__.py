728e6126f48f6eca25cbfeec86bf284544d11eb8
xx <lihongcheng.lhc@antgroup.com>
2024-04-19 10:03:17 +0800

    fix data load all at once

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -14,7 +14,9 @@
 # limitations under the License.
 
 from .risk_taxonomy import risk_taxonomy_zh, risk_taxonomy_en
-from .dataset import txt2txt_set, txt2img_set, imgtxt2txt_set
+from .dataset import txt2txt_set
+from .dataset import txt2img_set
+from .dataset import imgtxt2txt_set
 
 
 __all__ = [
