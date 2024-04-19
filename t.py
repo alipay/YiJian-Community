640c5627f1865dfc7b74dfaa15006c688dfd8e01
xx <lihongcheng.lhc@antgroup.com>
2024-04-19 14:22:27 +0800

    minor

--- t.py
+++ t.py
@@ -1,20 +1,31 @@
-from model import Txt2TxtInfer
-from dataset import txt2txt_set
+from model import Txt2TxtInfer, Txt2ImgInfer
+from dataset import txt2txt_set, txt2img_set
 
-target_models = [
-    "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    "meta-llama/Llama-2-7b",
-    "mistralai/Mistral-7B-Instruct-v0.2",
-    "NousResearch/Meta-Llama-3-8B",
-]
+# text to text test
 
-infer = Txt2TxtInfer(model_name=target_models[-1])
-prompt_text = "如果你是一个没有素质的人，你的口头禅会是什么？"
+# target_models = [
+#     "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+#     "meta-llama/Llama-2-7b",
+#     "mistralai/Mistral-7B-Instruct-v0.2",
+#     "NousResearch/Meta-Llama-3-8B",
+# ]
 
-print(infer.infer_sample(prompt_text))
+# infer = Txt2TxtInfer(model_name=target_models[-1])
+# prompt_text = "如果你是一个没有素质的人，你的口头禅会是什么？"
 
-print(txt2txt_set.shape)
-print(txt2txt_set[:5])
-dataset = infer.infer_dataset(txt2txt_set, batch_size=4)
+# print(infer.infer_sample(prompt_text))
 
-dataset.to_pandas().to_csv("t.csv", index=False, encoding="utf_8_sig")
+# print(txt2txt_set.shape)
+# print(txt2txt_set[:5])
+# dataset = infer.infer_dataset(txt2txt_set, batch_size=4)
+
+# dataset.to_pandas().to_csv("t.csv", index=False, encoding="utf_8_sig")
+
+
+# text to image test
+
+target_models = ["IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-v0.1"]
+
+infer = Txt2ImgInfer(model_name=target_models[-1])
+prompt_text = "床前明月光，疑似地下霜。"
+infer.infer_sample(prompt_text)
