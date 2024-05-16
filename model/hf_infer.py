14339549eb828ef0cf5d7425161ace826a6a704a
xx <lihongcheng.lhc@antgroup.com>
2024-05-16 17:35:25 +0800

    fix

--- model/hf_infer.py
+++ model/hf_infer.py
@@ -39,16 +39,6 @@ class HFTxt2TxtInfer(Infer):
         self.model = pipeline(
             "text-generation", model=model_name, device_map=device_map, **kwargs
         )
-        # if self.model.model.config.is_decoder:
-        print("*$#" * 66)
-        print(self.model.model.config.to_diff_dict())
-        print(self.model.model.config.is_decoder)
-        print(self.model.model.config.is_encoder_decoder)
-        print(self.model.model.config.get_config_dict())
-        print("*$#" * 66)
-        self.model.tokenizer.padding_side = "left"
-        if not self.model.tokenizer.pad_token:
-            self.model.tokenizer.pad_token_id = self.model.model.config.eos_token_id
 
     def infer_data(
         self,
@@ -71,6 +61,10 @@ class HFTxt2TxtInfer(Infer):
         max_new_tokens: int = MAX_NEW_TOKENS,
         **kwargs,
     ) -> Dataset:
+        if not self.model.tokenizer.pad_token:
+            self.model.tokenizer.pad_token_id = self.model.model.config.eos_token_id
+        if self.model.model.config.is_decoder:
+            self.model.tokenizer.padding_side = "left"
 
         response_texts = [
             res[0]["generated_text"]
