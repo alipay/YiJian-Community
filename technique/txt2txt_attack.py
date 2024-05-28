ed88d5ea7f5fd2a6a6ab82691256a5f0efcbef43
xx <lihongcheng.lhc@antgroup.com>
2024-05-28 13:10:57 +0800

    minor refactor

--- technique/txt2txt_attack.py
+++ technique/txt2txt_attack.py
@@ -17,7 +17,11 @@ from typing import List, Dict
 from pprint import pprint
 from datasets import Dataset
 from .base_attack import PromptAttack
-from .txt2txt_seeds import txt2txt_attacks
+from .txt2txt_seeds import (
+    txt2txt_attacks,
+    txt2txt_attack_names_zh,
+    txt2txt_attack_names_en,
+)
 
 
 class Txt2TxtAttack(PromptAttack):
@@ -27,16 +31,12 @@ class Txt2TxtAttack(PromptAttack):
         self.attacks = txt2txt_attacks
         if self.lang == "zh":
             print("当前支持的文生文攻击手法如下：")
-            print(
-                "介绍了解，帮忙写作，反向诱导，循序渐进，强制同意，长句溢出，错误前提，虚拟化，口令复述，代码攻击，隐晦知识，正反介绍，文本摘要"
-            )
+            pprint(txt2txt_attack_names_zh)
         else:
             print(
                 "The currently supported text to text adversarial techniques are as follows:"
             )
-            print(
-                "introduction, writing_assistant, reverse_induction, step_by_step, forced_consent, sentence_overflow, fallacious_premise, virtualization, repetition, code_attack, implicit_knowledge, pros_and_cons, text_summarization"
-            )
+            pprint(txt2txt_attack_names_en)
 
     def attack_data(self, data: str, techniques: List[str] = None) -> Dict[str, str]:
         if not techniques:
