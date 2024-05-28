198a3def9d30c930098048aed1f4767b9090bc3e
xx <lihongcheng.lhc@antgroup.com>
2024-05-28 10:55:17 +0800

    bugfix

--- technique/txt2txt_attack.py
+++ technique/txt2txt_attack.py
@@ -62,10 +62,10 @@ class Txt2TxtAttack(PromptAttack):
                     f"Unsupported attacks! The currently supported text to text adversarial techniques should be in the list of {list(self.attacks.keys())}"
                 )
             aug_data[attack_name] = self.attacker.infer_data(
-                self.attacks[attack_name](data, self.lang),
+                self.attacks[attack_name](data, lang=self.lang),
                 do_sample=True,
             )
-            print(attack_name, self.attacks[attack_name](data, self.lang))
+            print(attack_name, self.attacks[attack_name](data, lang=self.lang))
         return aug_data
 
     def attack_dataset(self, dataset: Dataset, techniques: List[str]) -> Dataset:
