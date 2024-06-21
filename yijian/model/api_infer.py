03403592468ef1b5d6650958c719cf57a746908a
xixun <lihongcheng.lhc@antgroup.com>
2024-06-21 11:07:05 +0800

    add more api infers

--- yijian/model/api_infer.py
+++ yijian/model/api_infer.py
@@ -29,12 +29,12 @@ class APIInfer(Infer):
 
     def infer_data(self, data: str):
         console.log(
-            "This class will be implemented in the next update or you are welcomed to make a contribution."
+            "This will be implemented in the next update or you are welcomed to make a contribution."
         )
 
     def infer_dataset(self, dataset: Dataset) -> Dataset:
         console.log(
-            "This class will be implemented in the next update or you are welcomed to make a contribution."
+            "This will be implemented in the next update or you are welcomed to make a contribution."
         )
 
 
@@ -52,8 +52,54 @@ class AnthropicTxt2TxtInfer(APIInfer):
         console.log("Reference: https://github.com/anthropics/anthropic-sdk-python")
 
 
-class CohereTxt2TxtInfer(Infer):
+class CohereTxt2TxtInfer(APIInfer):
 
     def __init__(self, model_path: str):
         super().__init__(model_path)
         console.log("Reference: https://github.com/cohere-ai/cohere-python")
+
+
+class TongyiQwenTxt2TxtInfer(APIInfer):
+
+    def __init__(self, model_path: str):
+        super().__init__(model_path)
+        console.log(
+            "Reference: https://help.aliyun.com/zh/dashscope/developer-reference/api-details"
+        )
+
+
+class MoonshotTxt2TxtInfer(APIInfer):
+
+    def __init__(self, model_path: str):
+        super().__init__(model_path)
+        console.log("Reference: https://platform.moonshot.cn/docs/api/chat#基本信息")
+
+
+class BaichuanTxt2TxtInfer(APIInfer):
+
+    def __init__(self, model_path: str):
+        super().__init__(model_path)
+        console.log("Reference: https://platform.baichuan-ai.com/docs/api")
+
+
+class StabilityAITxt2ImgInfer(APIInfer):
+
+    def __init__(self, model_path: str):
+        super().__init__(model_path)
+        console.log("Reference: https://platform.stability.ai/docs/api-reference")
+
+
+class OpenAITxt2ImgInfer(APIInfer):
+
+    def __init__(self, model_path: str):
+        super().__init__(model_path)
+        console.log("Reference: https://platform.openai.com/docs/guides/images")
+
+
+class MidJourneyTxt2ImgInfer(APIInfer):
+
+    def __init__(self, model_path: str):
+        super().__init__(model_path)
+        console.log(
+            "Reference: https://docs.midjourney.com/docs/quick-start or https://github.com/erictik/midjourney-api"
+        )
