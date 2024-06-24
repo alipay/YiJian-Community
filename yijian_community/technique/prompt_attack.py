5295223e5355cac5df94d3828f0c8a7158ef38fc
xixun <lihongcheng.lhc@antgroup.com>
2024-06-24 19:16:16 +0800

    add batch_size for attack_dataset

--- yijian_community/technique/prompt_attack.py
+++ yijian_community/technique/prompt_attack.py
@@ -32,7 +32,7 @@ from yijian_community.technique.txt2txt_seeds import (
     txt2txt_attack_names_zh,
     txt2txt_attacks,
 )
-from yijian_community.utils import console
+from yijian_community.utils import BATCH_SIZE, console
 
 
 class BasePromptAttack(ABC):
@@ -65,7 +65,9 @@ class BasePromptAttack(ABC):
         pass
 
     @abstractmethod
-    def attack_dataset(self, dataset: Dataset, techniques: List[str], **kwargs) -> Dataset:
+    def attack_dataset(
+        self, dataset: Dataset, techniques: List[str], **kwargs
+    ) -> Dataset:
         """attack against one datasets.Dataset.
 
         Args:
@@ -99,7 +101,10 @@ class TextPromptAttack(BasePromptAttack):
                 "当前支持的文生文攻击手法如下：\nThe currently supported text to text adversarial techniques are as follows:"
             )
             pprint(
-                [zh + "/" + en for zh, en in zip(txt2txt_attack_names_zh, txt2txt_attack_names_en)]
+                [
+                    zh + "/" + en
+                    for zh, en in zip(txt2txt_attack_names_zh, txt2txt_attack_names_en)
+                ]
             )
         elif self.target == "txt2img":
             self.attacks = txt2img_attacks
@@ -107,14 +112,19 @@ class TextPromptAttack(BasePromptAttack):
                 "当前支持的文生图攻击手法如下：\nThe currently supported text to image adversarial techniques are as follows:"
             )
             pprint(
-                [zh + "/" + en for zh, en in zip(txt2img_attack_names_zh, txt2img_attack_names_en)]
+                [
+                    zh + "/" + en
+                    for zh, en in zip(txt2img_attack_names_zh, txt2img_attack_names_en)
+                ]
             )
         else:
             raise ValueError(
                 f"Attack target can only be 'txt2txt' or 'txt2img', but {self.target} found!"
             )
 
-    def attack_data(self, data: str, techniques: List[str] = None, **kwargs) -> Dict[str, str]:
+    def attack_data(
+        self, data: str, techniques: List[str] = None, **kwargs
+    ) -> Dict[str, str]:
         """
 
         Args:
@@ -143,7 +153,13 @@ class TextPromptAttack(BasePromptAttack):
                 aug_data[attack_name] = self.attacker.infer_data(attack_seed, **kwargs)
         return aug_data
 
-    def attack_dataset(self, dataset: Dataset, techniques: List[str] = None, **kwargs) -> Dataset:
+    def attack_dataset(
+        self,
+        dataset: Dataset,
+        techniques: List[str] = None,
+        batch_size: int = BATCH_SIZE,
+        **kwargs,
+    ) -> Dataset:
         """
 
         Args:
@@ -170,7 +186,9 @@ class TextPromptAttack(BasePromptAttack):
             console.log(f"Using {attack_name} to augment prompt texts ...")
             dataset_with_seeds = dataset.map(
                 lambda row: {
-                    "aug_prompt": self.attacks[attack_name](row["prompt_text"], lang=self.lang),
+                    "aug_prompt": self.attacks[attack_name](
+                        row["prompt_text"], lang=self.lang
+                    ),
                     "technique": attack_name,
                 }
             ).select_columns(["aug_prompt", "technique", "references"])
@@ -187,6 +205,8 @@ class TextPromptAttack(BasePromptAttack):
                         dataset_with_seeds, target_column="aug_prompt", **kwargs
                     )
                     .select_columns(["response_text", "technique", "references"])
-                    .rename_columns({"response_text": "text_prompt", "technique": "source"})
+                    .rename_columns(
+                        {"response_text": "text_prompt", "technique": "source"}
+                    )
                 )
         return concatenate_datasets(seeds_list)
--- yijian_community/technique/txt2txt_seeds/__init__.py
+++ yijian_community/technique/txt2txt_seeds/__init__.py
@@ -15,16 +15,22 @@
 
 
 from yijian_community.technique.txt2txt_seeds.code_attack import code_attack
-from yijian_community.technique.txt2txt_seeds.fallacious_premise import fallacious_premise
+from yijian_community.technique.txt2txt_seeds.fallacious_premise import (
+    fallacious_premise,
+)
 from yijian_community.technique.txt2txt_seeds.forced_consent import forced_consent
-from yijian_community.technique.txt2txt_seeds.implicit_knowledge import implicit_knowledge
+from yijian_community.technique.txt2txt_seeds.implicit_knowledge import (
+    implicit_knowledge,
+)
 from yijian_community.technique.txt2txt_seeds.introduction import introduction
 from yijian_community.technique.txt2txt_seeds.pros_and_cons import pros_and_cons
 from yijian_community.technique.txt2txt_seeds.repetition import repetition
 from yijian_community.technique.txt2txt_seeds.reverse_induction import reverse_induction
 from yijian_community.technique.txt2txt_seeds.sentence_overflow import sentence_overflow
 from yijian_community.technique.txt2txt_seeds.step_by_step import step_by_step
-from yijian_community.technique.txt2txt_seeds.text_summarization import text_summarization
+from yijian_community.technique.txt2txt_seeds.text_summarization import (
+    text_summarization,
+)
 from yijian_community.technique.txt2txt_seeds.virtualization import virtualization
 from yijian_community.technique.txt2txt_seeds.writing_assistant import writing_assistant
 
