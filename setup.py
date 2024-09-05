3436a492df0ffba60930edd8ff621d7c22327eb0
xx <lihongcheng.lhc@antgroup.com>
2024-09-05 16:27:45 +0800

    change version to 0.1.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def long_description():
 
 setup(
     name="yijian-community",
-    version="0.1.0",
+    version="0.1.1",
     license="Apache 2.0",
     description="YiJian-Community",
     long_description=long_description(),
