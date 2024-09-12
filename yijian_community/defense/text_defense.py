21f6eac7ae4e7102510e463e21d8101cf1d4aef2
xx <lihongcheng.lhc@antgroup.com>
2024-09-12 19:51:57 +0800

    unset top_k

--- yijian_community/defense/text_defense.py
+++ yijian_community/defense/text_defense.py
@@ -50,7 +50,6 @@ class ThuCoaiShieldLM(Infer):
 
         self.generation_config = dict(
             temperature=1.0,
-            top_k=0,
             top_p=1.0,
             do_sample=False,
             num_beams=1,
